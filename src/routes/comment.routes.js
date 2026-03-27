441e9448315233c8ee952da5e05fba5c9c0b561d
Gaurav-alt-byte <gauravkushawaha60@gmail.com>
2026-03-27 19:06:38 +0530

    added edit comment and comment tweet route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authentication } from "../middlewares/auth.middleware.js";
-import { comment_creator } from "../controllers/comments.controller.js";
+import { comment_creator, comment_edit } from "../controllers/comments.controller.js";
 
 const router = Router();
 router.route("/create-comment/:ContentId").post(authentication , comment_creator);
+router.route("/edit-comment/:CommentId").patch(authentication , comment_edit);
 
 export default router
--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authentication } from "../middlewares/auth.middleware.js";
 import { createTweet, delete_Tweet, getTweetById, modify_tweet, tweetFeedGenrator } from "../controllers/tweet.controller.js";
 import { Optionalauthentication } from "../middlewares/optionalAuthentication.middleware.js";
-import { getTweetReplies } from "../controllers/comments.controller.js";
+import { comment_creator, getTweetReplies } from "../controllers/comments.controller.js";
 const router = Router()
 
 router.route("/tweet/all").get(Optionalauthentication,tweetFeedGenrator);
@@ -10,6 +10,7 @@ router.route("tweet/:TweetId").get(getTweetById);
 router.route("/tweet/:ContentId/replies").get(Optionalauthentication,getTweetReplies);
 //secured routes 
 router.route("/tweet/create").post(authentication , createTweet),
+router.route("/tweet/:ContentId/comment").post(authentication , comment_creator)
 router.route("/tweet/manage/delete/:TweetId").delete(authentication , delete_Tweet);
 router.route("/tweet/manage/update/:TweetId").patch(authentication , modify_tweet);
 export default router
